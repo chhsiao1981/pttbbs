7f2347ffa0778625eb1a5cd2bbd1d301b0386e8c
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2017-11-25 15:36:18 +0800

    intend to make it human-readable

--- util/pyutil/sep_main_comments.py
+++ util/pyutil/sep_main_comments.py
@@ -17,6 +17,30 @@ S_OK = 0
 S_ERR = 1
 
 
+def Big5(content):
+    """Try to encode to big5 from utf-content
+
+    Args:
+        content (str): content with utf-8 encoding
+
+    Returns:
+        str: content with big-5 encoding
+    """
+    return content.decode('utf-8').encode('big5')
+
+
+def Ctrl(content):
+    """Try to set as control-content
+
+    Args:
+        content (str): content to be prefixed as control
+
+    Returns:
+        str: prefixed with control-char
+    """
+    return '\x1b' + content
+
+
 def sep_main_comments(content):
     '''Try to separate among main-content, origin, from, and comments
         (正文)\r\n※ 發信站:([^換行]+)\r\n(※ 文章網址/※ 編輯/※ 轉錄者)\r\n(推/噓/→/※ username:轉錄至看板)(.*)
@@ -34,13 +58,53 @@ def sep_main_comments(content):
     Returns:
         Error, (str, str, str, str, str): error-code, (main-content, origin, from, first-comment, rest-comments)
     '''
-    re_match = re.match(r'(.*)\r\n\xa1\xb0 \xb5o\xabH\xaf\xb8:([^\r\n]+)\r\n((.*?)\r\n)?(\x1b\[1;37m\xb1\xc0|\x1b\[1;31m\xbcN|\x1b\[1;31m\xa1\xf7|\xa1\xb0 \x1b\[1;32m[0-9A-Za-z]+\x1b\[0;32m:\xc2\xe0\xbf\xfd\xa6\xdc\xac\xdd\xaaO)(.*?)\r\n(.*)', content, re.M | re.S)
+
+    main_wildcard = '.*'
+
+    origin_head = Big5('※ 發信站:')
+    origin_wildcard = '[^\r\n]+'
+
+    from_wildcard = '.*?'
+
+    bright_white = Ctrl('\[1;37m')
+    yellow = Ctrl('\[33m')
+    color_reset = Ctrl('\[m')
+
+    username = '[0-9A-Za-z]+'
+
+    recommend = Big5('推')
+    recommend_wildcard = bright_white + recommend + ' ' + yellow + username + color_reset + yellow + ':'
+
+    bright_red = Ctrl('\[1;31m')
+    boo = Big5('噓')
+    boo_wildcard = bright_red + boo + ' ' + yellow + username + color_reset + yellow + ':'
+
+    comment = Big5('→')
+    comment_wildcard = bright_red + comment + ' ' + yellow + username + color_reset + yellow + ':'
+
+    bright_green = Ctrl('\[1;32m')
+    green = Ctrl('\[0;32m')
+    forward_to = Big5('轉錄至看板')
+    forward_wildcard = Big5('※') + ' ' + bright_green + username + green + ':' + forward_to
+
+    first_comment_head_wildcard = recommend_wildcard + '|' + boo_wildcard + '|' + comment_wildcard + '|' + forward_wildcard
+    first_comment_wildcard = '.*?'
+
+    the_rest_comments_wildcard = '.*'
+
+    regex = '(' + main_wildcard + ')\r\n' + \
+        origin_head + '(' + origin_wildcard + ')\r\n' + \
+        '((' + from_wildcard + ')\r\n)?' + \
+        '(' + first_comment_head_wildcard + ')(' + first_comment_wildcard + ')\r\n' + \
+        '(' + the_rest_comments_wildcard + ')'
+
+    re_match = re.match(regex, content, re.M | re.S)
 
     if re_match is None:
         return S_ERR, ('', '', '', '', '')
 
     main_content = re_match.group(1)
-    origin = '\xa1\xb0 \xb5o\xabH\xaf\xb8:' + re_match.group(2)
+    origin = origin_head + re_match.group(2)
     the_from = re_match.group(4)
     first_comment = re_match.group(5) + re_match.group(6)
     the_rest_comments = re_match.group(7)
@@ -62,7 +126,18 @@ def sep_main(content):
     Returns:
         Error, (str, str, str): error-code, (main-content, origin, from)
     """
-    re_match = re.match(r'(.*)\r\n\xa1\xb0 \xb5o\xabH\xaf\xb8:([^\r\n]+)\r\n((.*?)\r\n)?', content, re.M | re.S)
+    main_wildcard = '.*'
+
+    origin_head = Big5('※ 發信站:')
+    origin_wildcard = '[^\r\n]+'
+
+    from_wildcard = '.*?'
+
+    regex = '(' + main_wildcard + ')\r\n' + \
+        origin_head + '(' + origin_wildcard + ')\r\n' + \
+        '((' + from_wildcard + ')\r\n)?'
+
+    re_match = re.match(regex, content, re.M | re.S)
 
     if re_match is None:
         return S_ERR, ('', '', '')
