6c8310e509ffc9401f933f2d734c32cdec53f172
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2017-12-12 15:13:49 -0500

    test_migrate_merge

--- tests/test_mbbsd/test_migrate_merge.cc
+++ tests/test_mbbsd/test_migrate_merge.cc
@@ -8,18 +8,18 @@ TEST(migrate_merge, migrate_1to3_get_line) {
     int bytes_in_new_line;
 
     // the simplest test.
-    printf("the simplest test");
+    printf("the simplest test\n");
     EXPECT_EQ(0, migrate_1to3_get_line(buf, 0, 14, line, 0, &bytes_in_new_line));
     EXPECT_EQ(14, bytes_in_new_line);
     EXPECT_EQ(0, strncmp(line, "testtesttest\r\n", 14));
 
     // test when reaching the end of the buf
-    printf("test when reaching the end of the buf");
+    printf("test when reaching the end of the buf\n");
     EXPECT_EQ(-1, migrate_1to3_get_line(buf, 14, 14, line, 0, &bytes_in_new_line));
     EXPECT_EQ(0, bytes_in_new_line);
 
     // test from middle of the buf. and no ending.
-    printf("test from middle of the buf. and no ending");
+    printf("test from middle of the buf. and no ending\n");
     EXPECT_EQ(-1, migrate_1to3_get_line(buf, 10, 22, line, 0, &bytes_in_new_line));
     EXPECT_EQ(12, bytes_in_new_line);
     EXPECT_EQ(0, strncmp(line, "testtesttest", 12));
