6c06f404bf2babbb3fcc4d4598a9a4d6651cca6b
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2017-12-12 15:59:52 -0500

    test_migrate_merge

--- tests/test_mbbsd/test_migrate_merge.cc
+++ tests/test_mbbsd/test_migrate_merge.cc
@@ -4,6 +4,7 @@
 TEST(migrate_merge, migrate_1to3_get_line) {
     char buf[] = "testtesttest\r\n";
     char buf2[] = "testtest\r\ntesttesttest";
+    char buf3[] = "testtest\r\ntesttesttest\r\n";
     char line[MIGRATE_MERGE_BUF_SIZE];
     int bytes_in_new_line;
 
@@ -20,9 +21,15 @@ TEST(migrate_merge, migrate_1to3_get_line) {
 
     // test from middle of the buf. and no ending.
     printf("test from middle of the buf. and no ending\n");
-    EXPECT_EQ(-1, migrate_1to3_get_line(buf, 10, 22, line, 0, &bytes_in_new_line));
+    EXPECT_EQ(-1, migrate_1to3_get_line(buf2, 10, 22, line, 0, &bytes_in_new_line));
     EXPECT_EQ(12, bytes_in_new_line);
     EXPECT_EQ(0, strncmp(line, "testtesttest", 12));
+
+    // test from middle of the buf. and no ending.
+    printf("test from middle of the buf. and with ending\n");
+    EXPECT_EQ(0, migrate_1to3_get_line(buf3, 10, 24, line, 0, &bytes_in_new_line));
+    EXPECT_EQ(14, bytes_in_new_line);
+    EXPECT_EQ(0, strncmp(line, "testtesttest\n\n", 14));
 }
 
 int main(int argc, char **argv) {
