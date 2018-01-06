22ad87be8e1b7b4de83b04c09e73e8a06391529a
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-01-06 13:55:57 -0500

    pttdb.c include bbs.h

--- tests/test_mbbsd/test_pttdb.cc
+++ tests/test_mbbsd/test_pttdb.cc
@@ -3,12 +3,22 @@
 
 TEST(pttdb, serialize_uuid_bson) {
     UUID uuid;
-    bzero(uuid, sizeof(UUID));
+    char *str;
+    char buf[MAX_BUF_SIZE];
+
+
+    bzero(uuid, sizeof(UUID));    
 
     bson_t uuid_bson;
     bson_init(&uuid_bson);
     
     Err error = _serialize_uuid_bson(uuid, &uuid_bson);
+    str = bson_as_canonical_extended_json (&uuid_bson, NULL);
+    strcpy(buf, str);
+
+    printf("str: %s", str);
+
+    bson_free (str);
 
     bson_destroy(&uuid_bson);
 
