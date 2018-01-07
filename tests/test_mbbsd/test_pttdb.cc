c02b3f30a5a843706d47f87ede30d7237a9b6558
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-01-07 00:36:58 -0500

    _DB_FORCE_DROP_COLLECTION

--- tests/test_mbbsd/test_pttdb.cc
+++ tests/test_mbbsd/test_pttdb.cc
@@ -146,7 +146,7 @@ void MyEnvironment::TearDown() {
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
-    ::testing::AddGlobalTestEnvironment(new MyEnvironment());
+    ::testing::AddGlobalTestEnvironment(new MyEnvironment);
 
     return RUN_ALL_TESTS();
 }
