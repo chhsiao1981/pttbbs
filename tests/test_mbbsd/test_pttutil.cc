7e88dd3af76b60a5e895ad1bcfb00736653e76cc
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-01-07 07:20:15 -0500

    test_pttutil

--- tests/test_mbbsd/test_pttutil.cc
+++ tests/test_mbbsd/test_pttutil.cc
@@ -8,6 +8,7 @@ TEST(pttutil, get_line_from_buf) {
     int offset_buf = 0;
     int offset_line = 0;
     int bytes_in_new_line = 0;
+    bzero(line, sizeof(line));
 
     strcpy(buf, "0123456789\r\nABCDEFGHIJ\r\n");
 
@@ -24,6 +25,7 @@ TEST(pttutil, get_line_from_buf_with_offset_buf) {
     int offset_buf = 12;
     int offset_line = 0;
     int bytes_in_new_line = 0;
+    bzero(line, sizeof(line));
 
     strcpy(buf, "0123456789\r\nABCDEFGHIJ\r\n");
 
@@ -40,6 +42,7 @@ TEST(pttutil, get_line_from_buf_with_line_offset) {
     int offset_buf = 0;
     int offset_line = 2;
     int bytes_in_new_line = 0;
+    bzero(line, sizeof(line));
 
     strcpy(buf, "0123456789\r\nABCDEFGHIJ\r\n");
     line[0] = '!';
@@ -49,4 +52,21 @@ TEST(pttutil, get_line_from_buf_with_line_offset) {
     EXPECT_EQ(S_OK, error);
     EXPECT_EQ(12, bytes_in_new_line);
     EXPECT_STREQ("!@0123456789\r\n", line);
+}
+
+TEST(pttutil, get_line_from_buf_not_end) {
+    int len_buf = 10;
+    char buf[MAX_BUF_SIZE];
+    char line[MAX_BUF_SIZE];
+    int offset_buf = 0;
+    int offset_line = 0;
+    int bytes_in_new_line = 0;
+    bzero(line, sizeof(line));
+
+    strcpy(buf, "0123456789");
+
+    Err error = get_line_from_buf(buf, offset_buf, len_buf, line, offset_line, &bytes_in_new_line);
+    EXPECT_EQ(S_ERR, error);
+    EXPECT_EQ(10, bytes_in_new_line);
+    EXPECT_STREQ("0123456789", line);
 }
