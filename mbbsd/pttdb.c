44d77c763f78dddbf75276426fcda3e55ac0ab66
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-01-06 22:51:01 -0500

    uuid_to_milli_timestamp

--- mbbsd/pttdb.c
+++ mbbsd/pttdb.c
@@ -98,6 +98,7 @@ db_set_if_not_exists(int collection, bson_t *key) {
     bson_init(&set_val);
     status = bson_append_document(&set_val, "$set", -1, key);
     if(!status) {
+        printf("something went wrong with set_val");
         bson_destroy(&set_val);
         return S_ERR;
     }
@@ -107,12 +108,14 @@ db_set_if_not_exists(int collection, bson_t *key) {
 
     status = bson_append_bool(&opts, "upsert", -1, is_upsert);
     if(!status) {
+        printf("something went wrong with upsert");
         bson_destroy(&set_val);
         bson_destroy(&opts);
         return S_ERR;
     }
     status = bson_append_document(&opts, "$setOnInsert", -1, key);
     if(!status) {
+        printf("something went wrong with setOnInsert");
         bson_destroy(&set_val);
         bson_destroy(&opts);
         return S_ERR;
@@ -122,6 +125,7 @@ db_set_if_not_exists(int collection, bson_t *key) {
     bson_init(&reply);
     status = mongoc_collection_update_one(MONGO_COLLECTIONS[collection], key, &set_val, &opts, &reply, &error);
     if(!status) {
+        printf("something went wrong with update_one");
         bson_destroy(&set_val);
         bson_destroy(&opts);
         bson_destroy(&reply);
@@ -130,6 +134,7 @@ db_set_if_not_exists(int collection, bson_t *key) {
 
     error_code = _bson_get_value_int32(&reply, "nUpserted", &n_upserted);
     if(error_code) {
+        printf("something went wrong with nUpserted");
         bson_destroy(&set_val);
         bson_destroy(&opts);
         bson_destroy(&reply);
