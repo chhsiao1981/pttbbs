c1ff48ab758e7991ebeff883b803a193e7c8f581
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-01-07 10:08:26 -0500

    test_pttdb_util

--- include/pttdb_internal.h
+++ include/pttdb_internal.h
@@ -22,8 +22,8 @@ Err _serialize_content_uuid_bson(UUID uuid, const char *key, int block_id, bson_
 /**********
  * Mongo
  **********/
-Err _db_set_if_not_exists(int collection, bson_t *key);
-Err _db_update_one(int collection, bson_t *key, bson_t *val, bool is_upsert);
+Err db_set_if_not_exists(int collection, bson_t *key);
+Err db_update_one(int collection, bson_t *key, bson_t *val, bool is_upsert);
 
 Err _bson_exists(bson_t *b, char *name);
 Err _bson_get_value_int32(bson_t *b, char *name, int *value);
