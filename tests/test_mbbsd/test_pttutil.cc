15a0503e30dbbb7610c36a99e4cea27ae1fe147f
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-01-07 07:17:04 -0500

    test_pttutil

--- tests/test_mbbsd/test_pttutil.cc
+++ tests/test_mbbsd/test_pttutil.cc
@@ -32,3 +32,21 @@ TEST(pttutil, get_line_from_buf_with_offset_buf) {
     EXPECT_EQ(12, bytes_in_new_line);
     EXPECT_STREQ("ABCDEFGHIJ\r\n", line);
 }
+
+TEST(pttutil, get_line_from_buf_with_line_offset) {
+    int len_buf = 24;
+    char buf[MAX_BUF_SIZE];
+    char line[MAX_BUF_SIZE];
+    int offset_buf = 0;
+    int offset_line = 2;
+    int bytes_in_new_line = 0;
+
+    strcpy(buf, "0123456789\r\nABCDEFGHIJ\r\n");
+    line[0] = '!'
+    line[1] = '@'
+
+    Err error = get_line_from_buf(buf, offset_buf, len_buf, line, offset_line, &bytes_in_new_line);
+    EXPECT_EQ(S_OK, error);
+    EXPECT_EQ(12, bytes_in_new_line);
+    EXPECT_STREQ("!@0123456789\r\n", line);
+}
