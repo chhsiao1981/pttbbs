7fb2954d7b0a5e06eaf9cddb2a1b70344788c560
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-01-07 07:04:40 -0500

    test_pttutil

--- tests/test_mbbsd/test_pttutil.cc
+++ tests/test_mbbsd/test_pttutil.cc
@@ -8,7 +8,7 @@ TEST(pttutil, get_line_from_buf) {
     int offset_line = 0;
     int bytes_in_new_line = 0;
 
-    strcpy(buf, "0123456789\r\n0123456789\r\n")
+    strcpy(buf, "0123456789\r\n0123456789\r\n");
     Err error = get_line_from_buf(buf, 0, len_buf, line, offset_line, &bytes_in_new_line);
     EXPECT_EQ(S_OK, error);
     EXPECT_EQ(12, bytes_in_new_line);
