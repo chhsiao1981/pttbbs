10fb75309b8570c2c2fd08c6751b19595a605914
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-02-23 08:36:55 -0500

    test find

--- temp.c
+++ temp.c
@@ -192,7 +192,7 @@ void test3() {
     bson_destroy(val2);
     bson_destroy(key2);
 
-    fprintf(stderr, "after bson_destroy p_str: %s\n", p_str);
+    //fprintf(stderr, "after bson_destroy p_str: %s\n", p_str);
 }
 
 int main() {
