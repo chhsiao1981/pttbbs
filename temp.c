92646ea3821bd1b391b3b16a82a4e1cccc12f59f
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-02-22 23:58:47 -0500

    temp.c

--- temp.c
+++ temp.c
@@ -133,6 +133,8 @@ void test3() {
 
     while (mongoc_cursor_next(cursor, &result)) {
         len++;
+
+        bson_destroy(result);
     }
 
     fprintf(stderr, "after mongoc_cursor_next: len: %d\n", len);
