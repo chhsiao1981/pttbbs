5d78b51a0303dabec061b9869ac63ded87501b9d
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-02-23 07:55:27 -0500

    test find

--- temp.c
+++ temp.c
@@ -148,10 +148,10 @@ void test3() {
         len++;
     }
 
-    status = bson_iter_init(&iter, result);
+    bson_iter_init(&iter, result);
 
     bson_iter_find_descendant(&iter, "test", &it_val);
-    bson_iter_binary(&it_val, BSON_SUBTYPE_BINARY, len, &result);
+    bson_iter_binary(&it_val, BSON_SUBTYPE_BINARY, len, result);
 
     //fprintf(stderr, "after mongoc_cursor_next: len: %d\n", len);
 
