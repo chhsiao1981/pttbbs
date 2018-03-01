d3fa874ee873491d9294d18168b8d3a0a530dc27
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-03-01 03:49:13 -0500

    test_pttdb.serialize_main_bson

--- include/util_db.h
+++ include/util_db.h
@@ -50,8 +50,8 @@ Err db_find_one(int collection, bson_t *key, bson_t *fields, bson_t *result);
 Err bson_exists(bson_t *b, char *name);
 Err bson_get_value_int32(bson_t *b, char *name, int *value);
 Err bson_get_value_int64(bson_t *b, char *name, long int *value);
-Err bson_get_value_bin_with_init(bson_t *b, char *name, char **value, int *len);
-Err bson_get_value_bin(bson_t *b, char *name, int max_len, char *value, int *len);
+Err bson_get_value_bin_with_init(bson_t *b, char *name, char **value, int *p_len);
+Err bson_get_value_bin(bson_t *b, char *name, int max_len, char *value, int *p_len);
 
 
 #ifdef __cplusplus
