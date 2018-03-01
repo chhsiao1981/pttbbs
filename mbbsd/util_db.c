55fc4c7d7bcf6a7367381627c81a9943b571d677
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-03-01 14:10:40 -0500

    test_delete_main

--- mbbsd/util_db.c
+++ mbbsd/util_db.c
@@ -263,19 +263,19 @@ db_find_one_with_fields(int collection, bson_t *key, char **fields, int n_fields
     bson_init(&b_fields);
 
     for(int i = 0; i < n_fields; i++) {
-        bson_status = bson_append_bool(b_fields, fields[i], -1, true);
+        bson_status = bson_append_bool(&b_fields, fields[i], -1, true);
         if(!bson_status) {
             bson_destroy(&b_fields);
             return S_ERR;
         }
     }
-    bson_status = bson_append_bool(b_fields, "_id", -1, false);
+    bson_status = bson_append_bool(&b_fields, "_id", -1, false);
     if(!bson_status) {
         bson_destroy(&b_fields);
         return S_ERR;
     }
 
-    error_code = db_find_one(collection, key, b_fields, result);
+    error_code = db_find_one(collection, key, &b_fields, result);
     if(error) {
         bson_destroy(&b_fields);
         return error_code;
