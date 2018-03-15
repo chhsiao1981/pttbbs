3a91a93ed1d6ba8e49cb901f44701bfb310269c9
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-03-15 01:36:41 -0400

    init_comment_buf and destroy_comment and associate_comment and dissociate_comment

--- include/pttdb_internal.h
+++ include/pttdb_internal.h
@@ -34,7 +34,10 @@ Err _split_contents_core_one_line(char *line, int bytes_in_line, UUID ref_id, UU
 
 Err _split_contents_deal_with_last_line_block(int bytes_in_line, char *line, UUID ref_id, UUID content_id, enum MongoDBId mongo_db_id, ContentBlock *content_block, int *n_line, int *n_block);
 
+Err _read_content_blocks_core(bson_t *key, int max_n_block, int block_id, enum MongoDBId mongo_db_id, char *buf, int max_buf_size, ContentBlock *content_blocks, int *n_block, int *len);
+Err _dynamic_read_content_blocks_core(bson_t *key, int max_n_block, int block_id, enum MongoDBId mongo_db_id, char *buf, int max_buf_size, ContentBlock *content_blocks, int *n_block, int *len);
 Err _read_content_blocks_get_db_results(bson_t **db_results, UUID content_id, int max_n_block, int block_id, enum MongoDBId mongo_db_id, int *n_block);
+
 Err _form_b_array_block_ids(int block_id, int max_n_block, bson_t *b);
 Err _sort_by_block_id(bson_t **db_results, int n_block);
 int _cmp_sort_by_block_id(const void *a, const void *b);
@@ -50,7 +53,6 @@ Err _deserialize_content_block_bson(bson_t *content_block_bson, ContentBlock *co
 Err _serialize_comment_bson(Comment *comment, bson_t **comment_bson);
 Err _deserialize_comment_bson(bson_t *comment_bson, Comment *comment);
 Err _get_comment_info_by_main_deal_with_result(bson_t *result, int n_result, int *n_total_comments, int *total_len);
-Err _dynamic_read_content_blocks_core(bson_t *key, int max_n_block, int block_id, enum MongoDBId mongo_db_id, char *buf, int max_buf_size, ContentBlock *content_blocks, int *n_block, int *len);
 
 /**********
  * CommentReply
