884212c7c23105b4c1f56cbb65626b6259b58b1b
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-03-15 15:29:45 -0400

    ReadCommentsOpType

--- tests/test_mbbsd/test_pttdb_comment.cc
+++ tests/test_mbbsd/test_pttdb_comment.cc
@@ -225,10 +225,16 @@ TEST(pttdb_comment, ensure_db_results_order) {
             );    
     }
 
-    Err error = _ensure_db_results_order(db_results, n_results, true);
+    Err error = _ensure_db_results_order(db_results, n_results, READ_COMMENTS_OP_TYPE_GT);
     EXPECT_EQ(S_ERR, error);
 
-    error = _ensure_db_results_order(db_results, n_results, false);
+    Err error = _ensure_db_results_order(db_results, n_results, READ_COMMENTS_OP_TYPE_GTE);
+    EXPECT_EQ(S_ERR, error);
+
+    error = _ensure_db_results_order(db_results, n_results, READ_COMMENT_OP_TYPE_LT);
+    EXPECT_EQ(S_ERR, error);
+
+    error = _ensure_db_results_order(db_results, n_results, READ_COMMENT_OP_TYPE_LTE);
     EXPECT_EQ(S_ERR, error);
 
     for(int i = 0; i < n_results; i++) {
@@ -248,10 +254,16 @@ TEST(pttdb_comment, ensure_db_results_order2) {
             );    
     }
 
-    Err error = _ensure_db_results_order(db_results, n_results, true);
+    Err error = _ensure_db_results_order(db_results, n_results, READ_COMMENT_OP_TYPE_GT);
     EXPECT_EQ(S_OK, error);
 
-    error = _ensure_db_results_order(db_results, n_results, false);
+    Err error = _ensure_db_results_order(db_results, n_results, READ_COMMENT_OP_TYPE_GTE);
+    EXPECT_EQ(S_OK, error);
+
+    error = _ensure_db_results_order(db_results, n_results, READ_COMMENT_OP_TYPE_LT);
+    EXPECT_EQ(S_ERR, error);
+
+    error = _ensure_db_results_order(db_results, n_results, READ_COMMENT_OP_TYPE_LTE);
     EXPECT_EQ(S_ERR, error);
 
     for(int i = 0; i < n_results; i++) {
@@ -271,10 +283,16 @@ TEST(pttdb_comment, ensure_db_results_order3) {
             );    
     }
 
-    Err error = _ensure_db_results_order(db_results, n_results, false);
+    Err error = _ensure_db_results_order(db_results, n_results, READ_COMMENT_OP_TYPE_LT);
     EXPECT_EQ(S_OK, error);
 
-    error = _ensure_db_results_order(db_results, n_results, true);
+    Err error = _ensure_db_results_order(db_results, n_results, READ_COMMENT_OP_TYPE_LTE);
+    EXPECT_EQ(S_OK, error);
+
+    error = _ensure_db_results_order(db_results, n_results, READ_COMMENT_OP_TYPE_GT);
+    EXPECT_EQ(S_ERR, error);
+
+    error = _ensure_db_results_order(db_results, n_results, READ_COMMENT_OP_TYPE_GTE);
     EXPECT_EQ(S_ERR, error);
 
     for(int i = 0; i < n_results; i++) {
