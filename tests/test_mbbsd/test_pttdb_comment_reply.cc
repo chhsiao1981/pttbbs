b9af743c467f394d609c47ffb0a92ec13de44ff6
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-03-15 22:10:12 -0400

    test_pttdb_comment_reply

--- tests/test_mbbsd/test_pttdb_comment_reply.cc
+++ tests/test_mbbsd/test_pttdb_comment_reply.cc
@@ -5,6 +5,68 @@
 #include "pttdb_internal.h"
 #include "util_db_internal.h"
 
+TEST(pttdb_comment_reply, serialize_comment_reply_bson) {
+    CommentReply comment_reply = {};
+    CommentReply comment_reply2 = {};
+
+    init_comment_reply_buf(&comment_reply);
+    init_comment_reply_buf(&comment_reply2);
+
+    comment_reply.version = 2;
+    gen_uuid(comment_reply.the_id);
+    memcpy(comment_reply.comment_id, comment_reply.the_id, UUIDLEN);
+    gen_uuid(comment_reply.main_id);
+    comment_reply.status = LIVE_STATUS_ALIVE;
+
+    strcpy(comment_reply.status_updater, "updater1");
+    strcpy(comment_reply.status_update_ip, "10.3.1.4");
+
+    strcpy(comment_reply.poster, "poster1");
+    strcpy(comment_reply.ip, "10.3.1.5");
+    comment_reply.create_milli_timestamp = 1514764800000; //2018-01-01 08:00:00 CST
+    strcpy(comment_reply.updater, "updater2");
+    strcpy(comment_reply.update_ip, "10.3.1.6");
+    comment_reply.update_milli_timestamp = 1514764801000; //2018-01-01 08:00:01 CST
+
+    strcpy(comment_reply.buf, "test_buf\r\n");
+    comment_reply.len = strlen(comment_reply.buf);
+    comment_reply.n_line = 1;
+
+    bson_t *comment_reply_bson = NULL;
+
+    Err error = _serialize_comment_reply_bson(&comment, &comment_reply_bson);
+    EXPECT_EQ(S_OK, error);
+
+    char *str = bson_as_canonical_extended_json(comment_reply_bson, NULL);
+    fprintf(stderr, "test_pttdb_comment_reply.serialize_comment_reply_bson: comment_reply_bson: %s\n", str);
+    bson_free(str);
+
+    error = _deserialize_comment_reply_bson(comment_reply_bson, &comment_reply2);
+
+    bson_safe_destroy(&comment_reply_bson);
+
+    EXPECT_EQ(S_OK, error);
+    EXPECT_EQ(comment_reply.version, comment_reply2.version);
+    EXPECT_EQ(0, strncmp((char *)comment_reply.the_id, (char *)comment_reply2.the_id, UUIDLEN));
+    EXPECT_EQ(0, strncmp((char *)comment_reply.comment_id, (char *)comment_reply2.comment_id, UUIDLEN));
+    EXPECT_EQ(0, strncmp((char *)comment_reply.main_id, (char *)comment_reply2.main_id, UUIDLEN));
+    EXPECT_EQ(comment_reply.status, comment_reply2.status);
+    EXPECT_STREQ(comment_reply.status_updater, comment_reply2.status_updater);
+    EXPECT_STREQ(comment_reply.status_update_ip, comment_reply2.status_update_ip);
+    EXPECT_STREQ(comment_reply.poster, comment_reply2.poster);
+    EXPECT_STREQ(comment_reply.ip, comment_reply2.ip);
+    EXPECT_EQ(comment_reply.create_milli_timestamp, comment_reply2.create_milli_timestamp);
+    EXPECT_STREQ(comment_reply.updater, comment_reply2.updater);
+    EXPECT_STREQ(comment_reply.update_ip, comment_reply2.update_ip);
+    EXPECT_EQ(comment_reply.update_milli_timestamp, comment_reply2.update_milli_timestamp);
+    EXPECT_EQ(comment_reply.len, comment_reply2.len);
+    EXPECT_EQ(comment_reply.n_line, comment_reply2.n_line);
+    EXPECT_STREQ(comment_reply.buf, comment_reply2.buf);
+
+    destroy_comment_reply(&comment_reply);
+    destroy_comment_reply(&comment_reply2);
+}
+
 /**********
  * MAIN
  */
