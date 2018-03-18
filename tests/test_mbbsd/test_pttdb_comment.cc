5f59d16f91feb164963180004d846c80383963bd
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-03-17 23:52:25 -0400

    test_pttdb.get_file_info_by_main_get_content_block_info

--- tests/test_mbbsd/test_pttdb_comment.cc
+++ tests/test_mbbsd/test_pttdb_comment.cc
@@ -1187,6 +1187,59 @@ TEST(pttdb_comment, read_comments_by_main5_LTE)
     }
 }
 
+TEST(pttdb_comment, get_newest_comment) {
+    _DB_FORCE_DROP_COLLECTION(MONGO_MAIN);
+    _DB_FORCE_DROP_COLLECTION(MONGO_MAIN_CONTENT);
+    _DB_FORCE_DROP_COLLECTION(MONGO_COMMENT);
+    _DB_FORCE_DROP_COLLECTION(MONGO_COMMENT_REPLY);
+
+    int fd = open("data_test/test1.txt", O_RDONLY);
+
+    aidu_t aid = 12345;
+    char board[IDLEN + 1] = {};
+    char title[TTLEN + 1] = {};
+    char poster[IDLEN + 1] = {};
+    char ip[IPV4LEN + 1] = {};
+    char origin[MAX_ORIGIN_LEN + 1] = {};
+    char web_link[MAX_WEB_LINK_LEN + 1] = {};
+    int len = 10020;
+    UUID main_id = {};
+    UUID content_id = {};
+
+    strcpy(board, "test_board");
+    strcpy(title, "test_title");
+    strcpy(poster, "test_poster");
+    strcpy(ip, "test_ip");
+    strcpy(origin, "ptt.cc");
+    strcpy(web_link, "http://www.ptt.cc/bbs/alonglonglongboard/M.1234567890.ABCD.html");
+
+    // create-main-from-fd
+    Err error = create_main_from_fd(aid, board, title, poster, ip, origin, web_link, len, fd, main_id, content_id);
+    EXPECT_EQ(S_OK, error);
+
+
+    close(fd);
+
+    // create-comment
+    UUID comment_id = {};
+    UUID comment_id2 = {};
+
+    error = create_comment(main_id, (char *)"poster1", (char *)"10.3.1.4", 10, (char *)"test1test1", COMMENT_TYPE_GOOD, comment_id);
+    EXPECT_EQ(S_OK, error);
+    error = create_comment(main_id, (char *)"poster2", (char *)"10.3.1.4", 10, (char *)"test2test2", COMMENT_TYPE_GOOD, comment_id2);
+
+
+    UUID newest_comment_id = {};
+    time64_t newest_create_milli_timestamp = 0;
+    char newest_poster[IDLEN + 1] = {};
+    int n_comment = 0;
+    // get newest comment
+    error = get_newest_comment(main_id, newest_comment_id, &newest_create_milli_timestamp, newest_poster, &n_comment);
+    EXPECT_EQ(S_OK, error);
+    EXPECT_STREQ((char *)"poster2", newest_poster);
+    EXPECT_EQ(2, n_comment);
+}
+
 /**********
  * MAIN
  */
@@ -1219,6 +1272,15 @@ void MyEnvironment::SetUp() {
         fprintf(stderr, "[ERROR] UNABLE TO init mongo collections\n");
         return;
     }
+
+    FILE *f = fopen("data_test/test1.txt", "w");
+    for (int j = 0; j < 10; j++) {
+        for (int i = 0; i < 1000; i++) {
+            fprintf(f, "%c", 64 + (i % 26));
+        }
+        fprintf(f, "\r\n");
+    }
+    fclose(f);    
 }
 
 void MyEnvironment::TearDown() {
