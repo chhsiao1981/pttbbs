d418b89868857901139853944142ebeee3ec1d75
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-03-21 09:34:15 -0400

    bson_get_value. TODO: 1. pass compile. 2. pass test. 3. more test

--- mbbsd/pttdb_comment.c
+++ mbbsd/pttdb_comment.c
@@ -528,7 +528,7 @@ dynamic_read_b_comment_comment_reply_by_ids_to_buf(bson_t **b_comments, int n_co
     // query
     if(!error_code) {
         error_code = read_comments_by_query_to_bsons(query_comment, comment_fields, n_comment, b_comment_contents, &n_comment_content);
-        if(n_comment != n_read_comment) error_code = S_ERR;
+        if(n_comment != n_comment_content) error_code = S_ERR;
     }
 
     if(!error_code) {
