be31cde133c327ae14567919e168c4202d2a74e7
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-03-21 12:37:00 -0400

    form_Rand_list

--- mbbsd/pttdb_misc.c
+++ mbbsd/pttdb_misc.c
@@ -115,15 +115,15 @@ _display_uuid(UUID uuid)
 }
 
 Err
-form_rand_list(int n, int **rand_list) {
-    *rand_list = malloc(sizeof(int) * n);
-    if(!rand_list) return S_ERR;
+form_rand_list(int n, int **p_rand_list) {
+    *p_rand_list = malloc(sizeof(int) * n);
+    if(!p_rand_list) return S_ERR;
 
-    int *tmp_rand_list = *rand_list;
+    int *rand_list = *p_rand_list;
 
-    int *tmp_available_pos = malloc(sizeof(int) * n);
+    int *available_pos = malloc(sizeof(int) * n);
     for(int i = 0; i < n; i++) {
-        tmp_available_pos[i] = i;
+        available_pos[i] = i;
     }
 
     int n_available_pos = n;
@@ -132,15 +132,15 @@ form_rand_list(int n, int **rand_list) {
     int tmp;        
     for(int i = 0; i < n; i++, n_available_pos--) {
         idx = random() % n_available_pos;
-        pos = tmp_available_pos[idx];
-        tmp_rand_list[pos] = i;
+        pos = available_pos[idx];
+        rand_list[pos] = i;
 
         fprintf(stderr, "pttdb_misc.form_rand_list: (%d/%d): idx: %d pos: %d n_available_pos: %d\n", i, n, idx, pos, n_available_pos);        
 
         // swap with last one
         tmp = pos;
-        tmp_available_pos[idx] = tmp_available_pos[n_available_pos - 1];
-        tmp_available_pos[n_available_pos - 1] = tmp;
+        available_pos[idx] = available_pos[n_available_pos - 1];
+        available_pos[n_available_pos - 1] = tmp;
     }
 
     fprintf(stderr, "pttdb_misc.form_rand_list: to safe_free\n");
