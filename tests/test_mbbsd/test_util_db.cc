599c6441e68ef0023bf66398eb680e222e8cacc9
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-03-21 20:29:09 -0400

    bsons_to_bson_dict_by_uu

--- tests/test_mbbsd/test_util_db.cc
+++ tests/test_mbbsd/test_util_db.cc
@@ -348,7 +348,7 @@ TEST(util_db, bsons_to_bson_dict_by_uu) {
     UUID the_id = {};
     char poster[IDLEN + 1] = {};
     for(int i = 0; i < n_bson; i++) {
-        sprintf(the_id, "id%03d", i);
+        sprintf((char *)the_id, "id%03d", i);
         sprintf(poster, "poster%03d", i);
         bsons[i] = BCON_NEW(
             (char *)"the_id", BCON_BINARY(the_id, UUIDLEN),
@@ -361,7 +361,7 @@ TEST(util_db, bsons_to_bson_dict_by_uu) {
     char key[100] = {};
     int len = 0;
     bson_t *bson_dict = NULL;
-    Err error = bsons_to_bson_dict_by_uu(bsons, n_bson, "the_id", &bson_dict);
+    Err error = bsons_to_bson_dict_by_uu(bsons, n_bson, (char *)"the_id", &bson_dict);
     EXPECT_EQ(S_OK, error);
     for(int i = 0; i < n_bson; i++) {
         sprintf(key, "id%03d.poster", i);
