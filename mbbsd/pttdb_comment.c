1ad995ac0038a39fdc37dc383d7c6e27b3ba12c8
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-03-22 02:22:07 -0400

    test dynamic_read_b_comment_comment_reply_by_ids_to_buf

--- mbbsd/pttdb_comment.c
+++ mbbsd/pttdb_comment.c
@@ -676,6 +676,8 @@ _dynamic_read_b_comment_comment_reply_by_ids_to_buf_core(bson_t **b_comments, in
 
     _display_dict_bson_by_uu(dict_comment_content, "pttdb_comment._dynamic_read_b_comment_comment_reply_by_ids_to_buf_core: dict_comment_content: ");
 
+    _display_dict_bson_by_uu(dict_comment_reply, "pttdb_comment._dynamic_read_b_comment_comment_reply_by_ids_to_buf_core: dict_comment_reply: ");
+
     *n_read_comment = n_comment;
     for(int i = 0; i < n_comment; i++, p_b_comments++, len_comment = 0, len_comment_reply = 0, len_read_comment = 0, len_read_comment_reply = 0, b_comment_content = NULL, b_comment_reply = NULL) {
         // get comment_id
