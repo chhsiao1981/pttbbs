90a61cc1ab2ef040b3c4de90264feb1e18d9a2d5
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-03-22 01:43:52 -0400

    Makefile

--- tests/test_mbbsd/test_pttdb_dict_bson_by_uu.cc
+++ tests/test_mbbsd/test_pttdb_dict_bson_by_uu.cc
@@ -15,7 +15,7 @@ TEST(pttdb_dict_bson_by_uu, add_to_dict_bson_by_uu) {
 
     bson_t *b = BCON_NEW(
         "the_id", BCON_BINARY(uuid, UUIDLEN),
-        "poster", BCON_BINARY("poster0", 7)
+        "poster", BCON_BINARY((unsigned char *)"poster0", 7)
         );
 
     error = add_to_dict_bson_by_uu(uuid, b, &dict_bson_by_uu);
@@ -23,11 +23,11 @@ TEST(pttdb_dict_bson_by_uu, add_to_dict_bson_by_uu) {
 
     char poster[IDLEN + 1] = {};
     bson_t *b2 = NULL;
-    error = get_bson_from_dict_bson_by_uu(&dict_bson_by_uu, uuid, b2);
+    error = get_bson_from_dict_bson_by_uu(&dict_bson_by_uu, uuid, &b2);
     EXPECT_EQ(b, b2);
     int len = 0;
-    error = bson_get_bin(b2, "poster", IDLEN, poster, &len);
-    EXPECT_STREQ(poster, "poster");
+    error = bson_get_value_bin(b2, "poster", IDLEN, poster, &len);
+    EXPECT_STREQ(poster, "poster0");
 
     // free
     bson_safe_destroy(&b);
@@ -48,15 +48,8 @@ public:
 };
 
 void MyEnvironment::SetUp() {
-    Err err = S_OK;
-
     FD = open("log.test_pttdb_dict_bson_by_uu.err", O_WRONLY | O_CREAT | O_TRUNC, 0660);
     dup2(FD, 2);
-
-    const char *db_name[] = {
-        "test_post",
-        "test",
-    };
 
 }
 
