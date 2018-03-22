286f224cbd1a61bbc2f5c7c853ce2028a05816d3
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-03-22 01:31:43 -0400

    Makefile

--- mbbsd/pttdb_comment.c
+++ mbbsd/pttdb_comment.c
@@ -672,6 +672,7 @@ _dynamic_read_b_comment_comment_reply_by_ids_to_buf_core(bson_t **b_comments, in
 
     bson_t *b_comment_content = NULL;
     bson_t *b_comment_reply = NULL;
+    Err error_code_b_comment_reply = S_OK;
     for(int i = 0; i < n_comment; i++, p_b_comments++, len_comment = 0, len_comment_reply = 0, len_read_comment = 0, len_read_comment_reply = 0, b_comment_content = NULL, b_comment_reply = NULL) {
         // get comment_id
         error_code = bson_get_value_bin(*p_b_comments, "the_id", UUIDLEN, comment_id, &len);
