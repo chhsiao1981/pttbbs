a4b2ea0676206358b8af90be9e12a1e13f7c5fff
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-03-27 12:00:58 -0500

    util_timestamp

--- mbbsd/util_timestamp.c
+++ mbbsd/util_timestamp.c
@@ -4,7 +4,10 @@ Err
 milli_timestamp_to_year(time64_t milli_timestamp, int *year)
 {
     struct tm tmp_tm = {};
-    localtime_r(&milli_timestamp, &tmp_tm);
+    time64_t timestamp = 0;
+
+    Err error_code = milli_timestamp_to_timestamp(milli_timestamp, &timestamp);
+    localtime_r(&timestamp, &tmp_tm);
     *year = tmp_tm.tm_year + 1900;
     return S_OK;
 }
