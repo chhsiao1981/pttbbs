db5e90aa16a11a8f13bd6f873a9c59b5f2415562
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-03-31 05:38:08 +0800

    test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -10,8 +10,9 @@ test_name=${BASH_ARGV[0]}
 
 for p in tests/test_common/test_sys tests/test_mbbsd
 do
-    if [ -x "${p}/${test_name}"]
+    if [ -x "${p}/${test_name}" ]
         then
+        echo "${p}/${test_name}:"
         "${p}/${test_name}"
     fi
 done
