bf9f9eccec12ef9768af7716128744b1aaff48ca
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-05-04 01:38:41 +0800

    vedit3_action_move_up

--- common/pttui/vedit3_action.c
+++ common/pttui/vedit3_action.c
@@ -429,7 +429,7 @@ _vedit3_action_move_up_ensure_top_of_window()
 {
     // XXX assuming current-buffer <= end-line-buffer
     // need to move down
-    if (VEDIT3_EDITOR_STATUS.current_line != b_lines - 1) return S_OK;
+    if (VEDIT3_EDITOR_STATUS.current_line != 0) return S_OK;
 
     UUID main_id = {};
     memcpy(main_id, VEDIT3_STATE.main_id, UUIDLEN);
