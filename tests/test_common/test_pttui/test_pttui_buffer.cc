087052d7c3e5e04c2f56f8da2d82e5ff64b067f1
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-05-08 18:53:54 +0800

    next_ne and pre_ne add head / tail check

--- tests/test_common/test_pttui/test_pttui_buffer.cc
+++ tests/test_common/test_pttui/test_pttui_buffer.cc
@@ -2190,7 +2190,7 @@ TEST(pttui_buffer, pttui_buffer_info_to_resource_info)
     fprintf(stderr, "test_pttui_buffer.pttui_buffer_info_to_resource_info: after loop buffer_info\n");
 
     PttUIResourceInfo resource_info = {};
-    error = _pttui_buffer_info_to_resource_info(buffer_info.head, &resource_info);
+    error = _pttui_buffer_info_to_resource_info(buffer_info.head, buffer_info.tail, &resource_info);
     EXPECT_EQ(S_OK, error);
 
     EXPECT_EQ(1, resource_info.queue[PTTDB_CONTENT_TYPE_MAIN * N_PTTDB_STORAGE_TYPE + PTTDB_STORAGE_TYPE_MONGO].n_queue);
