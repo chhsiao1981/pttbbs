44eb2b9faaae487c353518ceee8cb02a32e315f2
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-05-10 11:55:42 +0800

    get_content_block_from_file_core

--- common/pttui/pttui_resource_dict.c
+++ common/pttui/pttui_resource_dict.c
@@ -311,7 +311,7 @@ _pttui_resource_dict_add_data(UUID the_id, int block_id, int file_id, int len, c
     p->len = len;
     p->buf = buf;
 
-    fprintf(stderr, "pttui_resource_dict._pttui_resource_dict_add_data: content_type: %d comment_id: %d block_id: %d file_id: %d len: %d\n", content_type, comment_id, block_id, file_id, len);
+    fprintf(stderr, "pttui_resource_dict._pttui_resource_dict_add_data: the_idx: %d content_type: %d comment_id: %d block_id: %d file_id: %d len: %d\n", the_idx, content_type, comment_id, block_id, file_id, len);
 
     return S_OK;
 }
