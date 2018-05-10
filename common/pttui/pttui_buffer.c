c88a0ce4924c40a4808d9c24aaf56c2733443fc5
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-05-10 15:13:50 +0800

    pttui_id_comment_id_dict.h

--- common/pttui/pttui_buffer.c
+++ common/pttui/pttui_buffer.c
@@ -1490,6 +1490,7 @@ save_pttui_buffer_info_to_tmp_file(PttUIBufferInfo *buffer_info, FileInfo *file_
     if(!error_code) {
         error_code = _reset_pttui_buffer_info(buffer_info, file_info);
     }
+    fprintf(stderr, "pttui_buffer.save_pttui_buffer_info_to_tmp_file: after _reset_pttui_buffer_info: e: %d\n", error_code);
 
     error_code_lock = pttui_buffer_wrunlock_buffer_info(is_lock_buffer_info);
     if(!error_code && error_code_lock) error_code = error_code_lock;
@@ -1562,6 +1563,8 @@ _reset_pttui_buffer_info(PttUIBufferInfo *buffer_info, FileInfo *file_info)
         if(error_code) return error_code;
     }
 
+    fprintf(stderr, "pttui_buffer._reset_pttui_buffer_info: after set_load_line and set_file_offset: error_code: %d\n", error_code);
+
     p_buffer = p_buffer->next;
     p_pre_buffer = p_buffer;
 
@@ -1585,6 +1588,9 @@ _reset_pttui_buffer_info(PttUIBufferInfo *buffer_info, FileInfo *file_info)
         if(!error_code && error_code2) error_code = error_code2;
         error_code2 = _pttui_buffer_file_offset_next_from_content_block_info(p_buffer, p_pre_buffer, p_content_block);
         if(!error_code && error_code2) error_code = error_code2;
+
+        fprintf(stderr, "pttui_buffer._reset_pttui_buffer_info: (content-type: %d, comment-id: %d block-id: %d line-id: %d): load-line: (%d, %d, %d) file: (%d, %d, %d, %d)\n", p_buffer->content_type, p_buffer->comment_offset, p_buffer->block_offset, p_buffer->line_offset, p_buffer->load_line_offset, p_buffer->load_line_pre_offset, p_buffer->load_line_next_offset, p_buffer->file_offset, p_buffer->file_line_offset, p_buffer->file_line_pre_offset, p_buffer->file_line_next_offset);
+
         if(error_code) break;
 
         p_buffer->storage_type = p_content_block->storage_type;
