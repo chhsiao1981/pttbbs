1219dff3debd870fafe4063c235fd7cc36f6613f
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-05-12 00:13:13 +0800

    vedit3

--- common/pttdb/pttdb_content_block.c
+++ common/pttdb/pttdb_content_block.c
@@ -750,7 +750,7 @@ _construct_contents_from_content_block_infos_file_core(UUID main_id, enum PttDBC
 
     error_code = pttdb_file_get_data(main_id, content_type, orig_content_id, orig_block_id, file_id, &tmp_buf, &n_tmp_buf);
     char *disp_uuid = display_uuid(main_id);
-    char *disp_uuid2 = display_uuid2(orig_content_id);
+    char *disp_uuid2 = display_uuid(orig_content_id);
     fprintf(stderr, "pttdb_content_block._construct_contents_from_content_block_infos_file_core: main_id: %s orig_content_id: %s content_type: %d orig_block_id: %d file_id: %d n_tmp_buf: %d tmp_buf: %s\n", disp_uuid2, disp_uuid2, content_type, orig_block_id, file_id, n_tmp_buf, tmp_buf);
     free(disp_uuid2);
     free(disp_uuid);
