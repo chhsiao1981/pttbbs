05807bf43a901b474cef498bc51ef8d82956ee9c
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-05-12 11:59:09 +0800

    vedit3

--- include/cmpttui/pttui_thread_lock_private.h
+++ include/cmpttui/pttui_thread_lock_private.h
@@ -12,9 +12,9 @@ extern "C" {
 #include <time.h>
 #include <pthread.h>
 
-#define NS_DEFAULT_SLEEP_LOCK 10000000 // 10ms
-#define N_ITER_PTTUI_WRITE_LOCK 10 // write lock waits up to 100 ms
-#define N_ITER_PTTUI_READ_LOCK 5 // read lock waits up to 50 ms
+#define NS_DEFAULT_SLEEP_LOCK 4000000 // 4ms
+#define N_ITER_PTTUI_WRITE_LOCK 20 // write lock waits up to 80 ms
+#define N_ITER_PTTUI_READ_LOCK 10 // read lock waits up to 40 ms
 
 #ifdef __cplusplus
 }
