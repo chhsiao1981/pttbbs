4b80b4eccd6c8b4c44ac9d8f516771376574851f
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-05-12 23:20:14 +0800

    log

--- common/migrate_pttdb/parse_legacy_file_comment_create_milli_timestamp_reset_karma.c
+++ common/migrate_pttdb/parse_legacy_file_comment_create_milli_timestamp_reset_karma.c
@@ -7,11 +7,11 @@ _parse_legacy_file_comment_create_milli_timestamp_reset_karma(char *line, int by
     char *p_line = line;
 
     // to COMMENT_RESET_KARMA_INFIX
-    for(int i = 0; i < bytes_in_line && *p_line && *p_line != ' '; i++, p_line++);        
+    for(int i = 0; i < bytes_in_line && *p_line && *p_line != ' '; i++, p_line++); 
     p_line += LEN_COMMENT_RESET_KARMA_INFIX;
     // datetime
 
-    struct tm the_tm;
+    struct tm the_tm = {};
     char *ret = strptime(p_line, "%m/%d/%Y %H:%M:%S", &the_tm);
     if(ret == NULL) return S_ERR;
 
