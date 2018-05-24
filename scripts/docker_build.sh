e10270640cf7f6a8a875c2e30b7ecb9605a62959
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-05-24 16:39:48 +0800

    try to meet best practice for dockerfile

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,35 +1,27 @@
 #!/bin/bash
 
-if [ "${BASH_ARGC}" == "" ]
-then
-    MY_ARGC=0
-else
-    MY_ARGC=${BASH_ARGC}
-fi
-
-if [ "${MY_ARGC}" -gt "2" ]
+if [ "{BASH_ARGC}" != "" -a "${BASH_ARGC}" -gt "2" ]
 then
     echo "usage: docker_build.sh [[password]] [[tag]]"
     exit 255
 fi
 
-if [ "${MY_ARGC}" -lt "2" ]
+password='[TO_BE_REPLACED]'
+tag=`git branch|grep '*'|awk '{print $2}'`
+
+if [ "${BASH_ARGC}" == "1" ]
 then
-    tag=`git branch|grep '*'|awk '{print $2}'`
-else
+    password=${BASH_ARGV[0]}
+elif [ "${BASH_ARGC}" == "2" ]
+then
+    password=${BASH_ARGV[1]}
     tag=${BASH_ARGV[0]}
 fi
 
-if [ "${MY_ARGC}" == "1" ]
-then
-    password=${BASH_ARGV[0]}
-elif [ "${MY_ARGC}" == "2" ]
-then
-    password=${BASH_ARGV[1]}
-else
-    password='[TO_BE_REPLACED]'
-fi
+dockerfile="dockerfiles/Dockerfile.default"
 
-sed "s/\[TO_BE_REPLACED\]/${password}/g" Dockerfile.tmpl > Dockerfile
+sed -i "s/\[TO_BE_REPLACED\]/${password}/g" ${dockerfile}
 
-docker build -t pttbbs:${tag} .
+docker build -t pttbbs:${tag} -f ${dockerfile} .
+
+git check -- ${dockerfile}
