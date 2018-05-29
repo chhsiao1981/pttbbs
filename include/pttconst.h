3b7f430e2da77c62e7efc41dac9819e6a0eaf7c4
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-05-30 07:57:18 +0800

    pttgcc

--- include/pttconst.h
+++ include/pttconst.h
@@ -4,19 +4,10 @@
 
 #include "pttconst/ptterr.h"
 #include "pttconst/pttbool.h"
+#include "pttconst/pttgcc.h"
 
 #ifdef __cplusplus
 extern "C" {
-#endif
-
-#ifdef __GNUC__
-#define GCC_CHECK_FORMAT(a,b) __attribute__ ((format (printf, a, b)))
-#define GCC_NORETURN          __attribute__ ((__noreturn__))
-#define GCC_UNUSED            __attribute__ ((__unused__))
-#else
-#define GCC_CHECK_FORMAT(a,b)
-#define GCC_NORETURN
-#define GCC_UNUSED
 #endif
 
 #ifdef __cplusplus
