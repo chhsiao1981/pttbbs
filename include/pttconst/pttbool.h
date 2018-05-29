f20cea4897bee8caa9078124d34395799f943ade
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2018-05-30 07:35:48 +0800

    pttui_thread

--- include/pttconst/pttbool.h
+++ include/pttconst/pttbool.h
@@ -2,17 +2,13 @@
 #ifndef PTTBOOL_H
 #define PTTBOOL_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#ifndef __cplusplus
 
 typedef char bool;
 
 #define true 1
 #define false 0
 
-#ifdef __cplusplus
-}
 #endif
 
 #endif /* PTTBOOL_H */
