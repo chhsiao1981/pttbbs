8003d283794fe7a2131b7df923018fee281e7d31
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2022-04-23 10:24:22 -0400

    NotifyMe and NotifyUser

--- include/passwd_notify.hpp
+++ include/passwd_notify.hpp
@@ -4,7 +4,7 @@
 
 namespace passwdnotify {
 
-static void NotifyMe();
-static void NotifyUser(const std::string &userid, const std::string &fromhost, const std::string &email);
+void NotifyMe();
+void NotifyUser(const std::string &userid, const std::string &fromhost, const std::string &email);
 
 } //namespace
